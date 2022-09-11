76759598050181342ff974d308f78a51a2ff79e2
Andrew <ghostsly1@gmail.com>
2022-09-11 23:48:43 +0300

    2.7 solve some more chalenges

--- Challenges2.7/memoize.cpp
+++ Challenges2.7/memoize.cpp
@@ -1,6 +1,7 @@
 #include <functional>
 #include <iostream>
 #include <map>
+#include <cstdlib>
 
 template <typename T>
 class memFunctor
@@ -42,11 +43,76 @@ int square (int a)
     return a*a;
 }
 
+int random(int a)
+{
+    std::srand(a);
+    return std::rand();
+}
+
+int fact(int n)
+{
+    int i;
+    int result = 1;
+    for (i = 2; i <= n; ++i)
+    {
+        result *= i;
+    }
+    return result;
+}
+
+bool fOne()
+{
+    std::cout << "Hello!" << std::endl;
+    return true;
+}
+
+int fTwo(int x) 
+{
+    static int y = 0;
+    y += x;
+    return y;
+}
+
+bool boolOne(bool b)
+{
+    return true;
+}
+
+bool boolTwo(bool b)
+{
+    return false;
+}
+
+bool boolThree(bool b)
+{
+    return b;
+}
+
+bool boolFour(bool b)
+{
+    return !b;
+}
+
 int main()
 {
-    auto newFunc = memoize<int>(square);
-    std::cout << "newFunc answer: " << newFunc(4) << std::endl;
-    std::cout << "newFunc answer: " << newFunc(16) << std::endl;
-    std::cout << "newFunc answer: " << newFunc(4) << std::endl;
+    auto newFunc = memoize<int>(fTwo);
+    std::cout << "newFunc answer: " << newFunc(10) << std::endl;
+    std::cout << "newFunc answer: " << newFunc(2) << std::endl;
+    std::cout << "newFunc answer: " << newFunc(10) << std::endl;
+
     return 0;
 }
+
+//1) bool -> bool
+//   t -> t, f -> t (boolOne)
+//   t -> f, f -> f (boolTwo)
+//   t -> t, f -> f (boolThree)
+//   t -> f, f -> t (boolFour)
+//2) () -> bool
+//   () -> t (const True)
+//   () -> f (const False)
+//3) bool -> () (const ())
+//4) Void -> () (absurd)
+//   Void -> bool (absurd)
+//   Void -> Void (absurd)
+
