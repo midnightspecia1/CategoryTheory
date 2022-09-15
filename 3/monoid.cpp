7a0e1b797520a2480024dceec25462bf7250f805
midnightspecia1 <ghostsly1@gmail.com>
2022-09-15 23:23:13 +0300

    3: still trying to compile monoid

--- 3/monoid.cpp
+++ 3/monoid.cpp
@@ -1,14 +1,13 @@
 #include <string>
 #include <concepts>
-#include <type_traits>
-
+#include <iostream>
 
 
 template<class T>
 T mempty;
 
 template<class T>
-T mappend(T, T) = delete;
+T mappend(T, T);
 
 template<class M>
 concept Monoid = requires (M m)
@@ -27,5 +26,6 @@ std::string mappend(std::string s1, std::string s2)
 
 int main()
 {
+   std::cout << mappend("Hello", "world!") << std::endl;
    return 0; 
 }
